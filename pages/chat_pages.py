662a69ffc7662f441ee1dd8f39bee16b68242f53
talesitf <talesitf@al.insper.edu.br>
2024-11-25 15:23:18 -0300

    chat

--- pages/chat_pages.py
+++ pages/chat_pages.py
@@ -1,10 +1,23 @@
 import streamlit as st
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+all_messages = [
+        ('system', "Aqui está a questão do usuário: {user_query}"),
+        ('system', "Sempre responda no idioma português"),
+        ('system', "Toda vez que alguém fizer perguntas relacionadas ao Hub você deve responder em primeira pessoa no plural usando 'Somos','Fazemos”,'criamos”,'realizamos”,'executamos'."),
+    ]
+    
+llm = ChatOpenAI(temperature=0.05, model="gpt-4o-mini-2024-07-18")
+
+
+prompt = ChatPromptTemplate.from_messages(all_messages)
+
+chain = prompt| llm | StrOutputParser()
 
 # Verifica se o usuário está autenticado
-if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
-    st.error("Você precisa fazer login para acessar esta página.")
-    st.stop()
-
 # Página de chat
 st.title("Chatbot")
 
@@ -14,9 +27,9 @@ if "messages" not in st.session_state:
 # Exibir mensagens antigas
 for message in st.session_state["messages"]:
     if message["role"] == "user":
-        st.text_area("Você", message["text"], key=message["id"])
+        st.text_area("Você", message["text"], key=message["id"], height=40)
     else:
-        st.text_area("Bot", message["text"], key=message["id"])
+        st.text_area("Bot", message["text"], key=message["id"], height=40)
 
 # Entrada do usuário
 user_input = st.text_input("Digite sua mensagem:")
@@ -24,6 +37,6 @@ if st.button("Enviar"):
     if user_input:
         st.session_state["messages"].append({"role": "user", "text": user_input, "id": len(st.session_state["messages"])})
         # Resposta fictícia do bot
-        bot_response = f"Você disse: {user_input}"
+        bot_response = chain.invoke({'user_query':user_input})
         st.session_state["messages"].append({"role": "bot", "text": bot_response, "id": len(st.session_state["messages"]) + 1})
-
+        st.rerun()
