f1e046005eb0d32d44ad2fb548078d26c6a604ed
talesitf <talesitf@al.insper.edu.br>
2024-11-25 17:38:52 -0300

    kkk

--- pages/chat_pages.py
+++ pages/chat_pages.py
@@ -1,7 +1,10 @@
 import streamlit as st
+import os
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 all_messages = [
         ('system', "Aqui está a questão do usuário: {user_query}"),
